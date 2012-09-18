9f073ab82eb4a88fe571a696432b1c90267cbda3
suchasplus <suchasplus@gmail.com>
2012-09-18 18:39:59 +0800

    update 4.11

---
+++ 4/11.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    //int *ip;
+    
+    //string s, *sp = 0;
+    
+    //int i; double *dp = &i;
+
+    //int* ip, ip2;
+
+    //const int i=0, *p =i;
+
+    string *p = NULL;
+
+    return 0;
+}
